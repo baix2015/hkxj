6608eaaac854e9c87ecc6f5fdb0af5271b1b1b26
liuqi <1733119087@qq.com>
2020-02-12 10:16:17 +0800

    逆向工程创建wetchat_openid新表的类

---
+++ src/main/java/cn/hkxj/platform/mapper/WechatOpenidMapper.java
@@ -0,0 +1,30 @@
+package cn.hkxj.platform.mapper;
+
+import cn.hkxj.platform.pojo.WechatOpenid;
+import cn.hkxj.platform.pojo.WechatOpenidExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface WechatOpenidMapper {
+    int countByExample(WechatOpenidExample example);
+
+    int deleteByExample(WechatOpenidExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(WechatOpenid record);
+
+    int insertSelective(WechatOpenid record);
+
+    List<WechatOpenid> selectByExample(WechatOpenidExample example);
+
+    WechatOpenid selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") WechatOpenid record, @Param("example") WechatOpenidExample example);
+
+    int updateByExample(@Param("record") WechatOpenid record, @Param("example") WechatOpenidExample example);
+
+    int updateByPrimaryKeySelective(WechatOpenid record);
+
+    int updateByPrimaryKey(WechatOpenid record);
+}
