104c5be7102d7eabede0ea841d43ed69763b7553
liuqi <1733119087@qq.com>
2020-02-16 11:32:46 +0800

    修改service层代码，并向类里面添加了两个方法发

--- src/main/java/cn/hkxj/platform/mapper/WechatOpenidMapper.java
+++ src/main/java/cn/hkxj/platform/mapper/WechatOpenidMapper.java
@@ -32,4 +32,11 @@ public interface WechatOpenidMapper {
     int updateByPrimaryKeySelective(WechatOpenid record);
 
     int updateByPrimaryKey(WechatOpenid record);
+
+    //添加判断是否绑定的方法仿照OpenidPlusMapper编码
+    int isOpenidBind(String openid);
+    // 仿照OpenidPlusMapper编码
+    int openidUnbind(String openid);
+    //后添加方法仿照OpenidPlusMapper里面的getAllOpenidsFromOneClass方法，用于OpenidService
+    List<String> getAllOpenidsFromOneClass(@Param("classId") int classId, @Param("openid")String openid);
 }
